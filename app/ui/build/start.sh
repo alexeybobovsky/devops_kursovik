e710b323a75ff6bc5a9b08260602885329daf032
alexeybobovsky <alexey.bobovsky@gmail.com>
2022-02-21 10:19:59 +0000

    some update

--- app/ui/build/start.sh
+++ app/ui/build/start.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-export MONGO="mongo"
-export MONGO_PORT=27017
+
+#export MONGO="mongo"
+#export MONGO_PORT=27017
+
 cd search_engine_ui/ui
 FLASK_APP=ui.py gunicorn ui:app -b 0.0.0.0
